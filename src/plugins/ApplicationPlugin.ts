cc75cf06be6ff9bc15496064b6ebc79a7ff7edfc
shifenghu <chinashifenghu@gmail.com>
2023-07-21 09:15:39 +0800

    添加了内容

--- src/plugins/ApplicationPlugin.ts
+++ src/plugins/ApplicationPlugin.ts
@@ -6,7 +6,6 @@ export default class ApplicationPlugin extends MyPlugin {
   public InitializeListeners(_: SandboxConfigurator, context: SandboxContext) {
     this.Context = context;
     context.On("SetInfo", (message: Sandbox.IMessagePayload) => this.OnSetInfo(message));
-    context.On("GetName", (message: Sandbox.IMessagePayload) => context.Reply(message, ["Get name is Successed"]));
   }
 
   private OnSetInfo(message: Sandbox.IMessagePayload) {
